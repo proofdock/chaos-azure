abac2e75338dd1ab10d2d7dcdedd4f5778f64a2d
proofdock-support <support@proofdock.io>
2020-05-21 12:51:06 +0000

    Bump version: 0.8.11-dev2 → 0.8.11-dev3 [skip ci]

--- pdchaosazure/__init__.py
+++ pdchaosazure/__init__.py
@@ -18,7 +18,7 @@ from pdchaosazure.common.config import load_configuration, load_secrets
 __all__ = [
     "__version__", "discover", "init_client", "init_resource_graph_client"
 ]
-__version__ = '0.8.11-dev2'
+__version__ = '0.8.11-dev3'
 
 
 def discover(discover_system: bool = True) -> Discovery:
