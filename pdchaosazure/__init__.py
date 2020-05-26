7ae1aa41688c52b620068fe55381832e6957a9c6
proofdock-support <support@proofdock.io>
2020-05-26 14:28:14 +0000

    Bump version: 0.8.12-dev5 → 0.8.12-dev6 [skip ci]

--- pdchaosazure/__init__.py
+++ pdchaosazure/__init__.py
@@ -14,7 +14,7 @@ from pdchaosazure.common.config import load_subscription_id, load_secrets  # noq
 __all__ = [
     "__version__", "auth", "discover", "load_secrets", "load_subscription_id"
 ]
-__version__ = '0.8.12-dev5'
+__version__ = '0.8.12-dev6'
 __package__ = "proofdock-chaos-azure"
 
 
